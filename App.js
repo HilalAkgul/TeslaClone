b32b7a142fcbfacbd5520f11612b349b13a2af5c
Hilal Akgül <hilalakgul9393@outlook.com>
2021-05-17 21:09:52 +0300

    First upload

---
+++ App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { StyleSheet, View , StatusBar} from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <CarsList />
+    <StatusBar  barStyle="light-content"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
